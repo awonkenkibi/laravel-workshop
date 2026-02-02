472c9da6ed8aa4162cc3beeadef0504265c7adc8
awonkenkibi <awonkenkibi7@gmail.com>
2026-02-02 14:13:26 +0200

    updating Dockerfile

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -e
+
+# Set permissions for storage and cache directories
+chmod -R 777 storage bootstrap/cache
 
 Clear Laravel caches at runtime
 php artisan config:clear
@@ -9,4 +13,4 @@ php artisan cache:clear
 php artisan optimize
 
 # Run Laravel development server
-php artisan serve --host=0.0.0.0 --port=$PORT
+php artisan serve --host=0.0.0.0 --port=${PORT:-10000}
